4f87a225c382c628f6e79b4d1988dbcb48109ee5
DoItReal <iliya.yankow@gmail.com>
2023-12-09 14:21:24 +0200

    Working on imageDrawQueue

--- src/DesignEditor/LabelCanvas.tsx
+++ src/DesignEditor/LabelCanvas.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { UnifiedDesign, textFieldDesign, imageFieldDesign, HandleType, Position, TtextParameter, textParametersMap, Dimensions } from './Editor'; // Import the Design type
 import { styled } from '@mui/system';
 import { labelDataType } from '../db';
+import { png } from '../labels';
 const StyledCanvas = styled('canvas')`
   border: 1px solid #000;
   margin-bottom: 10px;
@@ -100,7 +101,7 @@ const Canvas: React.FC<CanvasProps> = ({ dimensions, designs, label }) => {
 
         return fitText();
     }
-    const drawQueue = (context: CanvasRenderingContext2D, renderQueue: { text: string; x: number; y: number, position:Position }[]) => {
+    const drawTextQueue = (context: CanvasRenderingContext2D, renderQueue: { text: string; x: number; y: number, position:Position }[]) => {
         const margin = 1.1;
         context.textBaseline = 'top';
 
@@ -112,6 +113,76 @@ const Canvas: React.FC<CanvasProps> = ({ dimensions, designs, label }) => {
             });
     }
 
+    const imagesCalibrateCenter = (design: UnifiedDesign, context: CanvasRenderingContext2D, imageURLs: string[] | number[]) => {
+        const renderQueue: { image: number; x: number; y: number, width: number, height: number, position: Position }[] = [];
+        const border = 2; //to add it to design.border
+        const generateAllergens = (arr: number[]) => {
+    if (arr.length === 1 && arr[0] === 0) return;
+    //calibrate and set this.fontSize
+    let textSize = imgCalibrate(arr);
+    textSize *= 2;
+    let dim = imgCenter(arr, textSize);
+      let dx = (design.dimensions.width - (arr.length * textSize * 2)) / 2;
+      let dy = (design.dimensions.height - border-textSize)/2;
+            //let dy = dim.y;
+            let { x, y } = dim;
+    let dWidth = textSize;
+    let dHeight = textSize;
+
+    for (let i = 0; i < arr.length; i++) {
+        context.font = textSize + "px " + design.font.split(' ')[1];
+        context.fillStyle = "blue";
+        renderQueue.push({ image: arr[i], x: dx + textSize / 2, y:dy, width:dWidth, height:dHeight, position: design.position });
+       // context.fillText(String(arr[i]), dx, dy + textSize * 0.9);
+      //  context.drawImage(png.images[Number(arr[i] - 1)], dx + textSize / 2, dy, dWidth, dHeight)
+        dx += textSize * 2;
+
+             }
+        textSize = parseInt(design.font);
+}
+    const imgCalibrate = (arr: Array<number>) => { // return the required fontSize to fit the image in the design
+        let textSize = parseInt(design.font);
+        const width = design.dimensions.width;
+        const height = design.dimensions.height;
+        
+    let wholeSize = arr.length * textSize * 4;
+    while (wholeSize > width - (20)) {
+        textSize--;
+        wholeSize = arr.length * textSize * 4;
+    }
+    return textSize;
+}
+    const imgCenter = (arr: Array<number>, textSize:number) => {
+        const width = design.dimensions.width - border*2;
+        const height = design.dimensions.height - border*2;
+        
+    let wholeSize = arr.length * textSize * 2;
+    let dx = (width - wholeSize)/2;
+    if (width > wholeSize) dx = (width - wholeSize) / 2;
+    else dx = (wholeSize - width) / 2;
+    let dy = textSize * 1.5 + height * 0.25;
+    dy = (dy - border) / 2 - textSize;
+    return { x: dx, y: dy };
+        }
+        generateAllergens(imageURLs as number[]);
+        return renderQueue;
+    }
+    const drawImageQueue = (context: CanvasRenderingContext2D, renderQueue: { image: number; x: number; y: number,width:number,height:number, position: Position }[]) => {
+        const margin = 1.1;
+        context.textBaseline = 'top';
+        const textSize = parseInt(context.font);
+        renderQueue.forEach(item => {
+            context.save();
+            context.translate(item.position.x, item.position.y);
+                context.fillText(String(item.image), item.x, item.y);
+                typeof(item.image) === 'number' && context.drawImage(png.images[Number(item.image - 1)], item.x + textSize / 2, item.y, item.width, item.height);
+            
+            context.restore();
+        });
+
+     //   context.fillText(String(arr[i]), dx, dy + textSize * 0.9);
+      //  context.drawImage(png.images[Number(arr[i] - 1)], dx + textSize / 2, dy, dWidth, dHeight)
+    }
     const drawDesign = (design: UnifiedDesign, context: CanvasRenderingContext2D) => {
         const { x, y } = design.position;
         context.save();
@@ -130,13 +201,23 @@ const Canvas: React.FC<CanvasProps> = ({ dimensions, designs, label }) => {
                 text = '';
             }
             const queue = txtCalibrateCenter(design, context, text);
-            queue && drawQueue(context, queue);
+            queue && drawTextQueue(context, queue);
         } else if ('type' in design) { // TO DO NEXT!! 
-            text = design.type; // there has to be way of getting the image and drawing it! 
+            if (design.type === 'allergens') {
+                const queue = imagesCalibrateCenter(design, context, label.allergens);
+                drawImageQueue(context, queue);
+            } else if (design.type === 'image') {
+                text = design.type; // there has to be way of getting the image and drawing it! 
+
+            } else {
+                text = 'undefined';
+                const queue = txtCalibrateCenter(design, context, text);
+                queue && drawTextQueue(context, queue);
+            }
         } else {
             text = 'Undefined';
             const queue = txtCalibrateCenter(design, context, text);
-            queue && drawQueue(context, queue);
+            queue && drawTextQueue(context, queue);
         }
        
         
